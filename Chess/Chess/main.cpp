602246fec900f70f792b3588aec3172c08420c1b
HaykTheKhulyan <hkhulyan@gmail.com>
2018-09-13 22:17:00 -0700

    Add piece textures to initializeBoard function

--- Chess/Chess/main.cpp
+++ Chess/Chess/main.cpp
@@ -1,6 +1,6 @@
 #include <SFML/Graphics.hpp>
 
-void initializeBoard(sf::Sprite &board, sf::Texture &boardTexture, sf::RectangleShape &boardBorder);
+void initializeBoard(sf::Sprite &board, sf::Texture &boardTexture, sf::RectangleShape &boardBorder, sf::Texture &pawn1Texture, sf::Texture &pawn2Texture, sf::Texture &knight1Texture, sf::Texture &knight2Texture, sf::Texture &bishop1Texture, sf::Texture &bishop2Texture, sf::Texture &rook1Texture, sf::Texture &rook2Texture, sf::Texture &queen1Texture, sf::Texture &queen2Texture, sf::Texture &king1Texture, sf::Texture &king2Texture);
 
 int main() {
 	sf::RenderWindow window(sf::VideoMode(1200, 800), "Chess", sf::Style::Close | sf::Style::Titlebar);
@@ -9,13 +9,6 @@ int main() {
 	sf::Texture boardTexture;
 	sf::RectangleShape boardBorder;
 
-
-	initializeBoard(board, boardTexture, boardBorder);
-
-
-
-	
-	/* Move these into the initialize function
 	sf::Texture pawn1Texture;
 	sf::Texture pawn2Texture;
 	sf::Texture knight1Texture;
@@ -29,22 +22,7 @@ int main() {
 	sf::Texture king1Texture;
 	sf::Texture king2Texture;
 
-	pawn1Texture.loadFromFile("Resources/pawn1.png");
-	pawn2Texture.loadFromFile("Resources/pawn2.png");
-	knight1Texture.loadFromFile("Resources/knight1.png");
-	knight2Texture.loadFromFile("Resources/knight2.png");
-	bishop1Texture.loadFromFile("Resources/bishop1.png");
-	bishop2Texture.loadFromFile("Resources/bishop2.png");
-	rook1Texture.loadFromFile("Resources/rook1.png");
-	rook2Texture.loadFromFile("Resources/rook2.png");
-	queen1Texture.loadFromFile("Resources/queen1.png");
-	queen2Texture.loadFromFile("Resources/queen2.png");
-	king1Texture.loadFromFile("Resources/king1.png");
-	king2Texture.loadFromFile("Resources/king2.png");
-
-	*/
-
-
+	initializeBoard(board, boardTexture, boardBorder, pawn1Texture, pawn2Texture, knight1Texture, knight2Texture, bishop1Texture, bishop2Texture, rook1Texture, rook2Texture, queen1Texture, queen2Texture, king1Texture, king2Texture);
 
 	float deltaTime = 0.0f;
 	sf::Clock clock;
@@ -74,7 +52,7 @@ int main() {
 	return 0;
 }
 
-void initializeBoard(sf::Sprite &board, sf::Texture &boardTexture, sf::RectangleShape &boardBorder) {
+void initializeBoard(sf::Sprite &board, sf::Texture &boardTexture, sf::RectangleShape &boardBorder, sf::Texture &pawn1Texture, sf::Texture &pawn2Texture, sf::Texture &knight1Texture, sf::Texture &knight2Texture, sf::Texture &bishop1Texture, sf::Texture &bishop2Texture, sf::Texture &rook1Texture, sf::Texture &rook2Texture, sf::Texture &queen1Texture, sf::Texture &queen2Texture, sf::Texture &king1Texture, sf::Texture &king2Texture) {
 	boardTexture.loadFromFile("Resources/board.png");
 
 	board.setTexture(boardTexture);
@@ -87,4 +65,17 @@ void initializeBoard(sf::Sprite &board, sf::Texture &boardTexture, sf::Rectangle
 	boardBorder.setOutlineThickness(4.0f);
 	boardBorder.setOutlineColor(sf::Color(170, 100, 39));
 	boardBorder.setPosition(sf::Vector2f(400.0f, 400.0f));
+
+	pawn1Texture.loadFromFile("Resources/pawn1.png");
+	pawn2Texture.loadFromFile("Resources/pawn2.png");
+	knight1Texture.loadFromFile("Resources/knight1.png");
+	knight2Texture.loadFromFile("Resources/knight2.png");
+	bishop1Texture.loadFromFile("Resources/bishop1.png");
+	bishop2Texture.loadFromFile("Resources/bishop2.png");
+	rook1Texture.loadFromFile("Resources/rook1.png");
+	rook2Texture.loadFromFile("Resources/rook2.png");
+	queen1Texture.loadFromFile("Resources/queen1.png");
+	queen2Texture.loadFromFile("Resources/queen2.png");
+	king1Texture.loadFromFile("Resources/king1.png");
+	king2Texture.loadFromFile("Resources/king2.png");
 }
